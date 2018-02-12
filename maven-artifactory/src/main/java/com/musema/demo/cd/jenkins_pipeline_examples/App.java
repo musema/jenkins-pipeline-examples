60348b4e9feda3aea772dcfd05fe49facd1a3331
Musema Hassen <musema.hassen@gmail.com>
2018-02-12 15:55:55 -0500

    add maven-artifactory config

---
+++ maven-artifactory/src/main/java/com/musema/demo/cd/jenkins_pipeline_examples/App.java
@@ -0,0 +1,13 @@
+package com.musema.demo.cd.jenkins_pipeline_examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
